c02d4a52d91399a74fc70740f317c37271bdd4a1
ElliottZaki <spudnick99@hotmail.com>
2022-02-03 12:21:29 +0000

    The differences between textContent and innerText Js

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const body = document.body
-const div = document.createElement('div')
+// const body = document.body
+// const div = document.createElement('div')
 
-div.innerText = 'Hello world!'
+// div.innerText = 'Hello world!'
+// div.textContent = 'Hello world2!'
 
-body.appendChild(div)
+// body.appendChild(div)
+
+const div = document.querySelector('div')
+
+console.log(div.textContent);
+console.log(div.innerText);
